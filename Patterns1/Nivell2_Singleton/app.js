09c5937db8c0c1f9e8b32733db6fec4fabcea648
JessFala <jessicafala@gmail.com>
2022-07-20 12:12:42 +0200

    Corregit

--- Patterns1/Nivell2_Singleton/app.js
+++ Patterns1/Nivell2_Singleton/app.js
@@ -11,24 +11,23 @@ let jugadorx3 = new Jugadorx("Terry");
 
 //Afegir Jugadorxs
 
-joc1.join(jugadorx1);
-joc1.join(jugadorx2);
-joc1.join(jugadorx3);
+joc1.afegirJugadorx(jugadorx1);
+joc1.afegirJugadorx(jugadorx2);
+joc1.afegirJugadorx(jugadorx3);
  
 
 //Mostrar Jugadorxs
-console.log (joc1.joc.jugadorx1);
-console.log (joc1.joc.jugadorx2);
-console.log (joc1.joc.jugadorx3);
+
+console.log(joc1.marcador.jugadorxs);
 
 //Mostrar puntuació abans d'iniciar la partida
-
-joc1.deductPunts(jugadorx1);
-joc1.deductPunts(jugadorx2);
-joc1.deductPunts(jugadorx3);
+joc1.sumarPunts(jugadorx1)
+joc1.restarPunts(jugadorx1);
+joc1.restarPunts(jugadorx2);
+joc1.restarPunts(jugadorx3);
 
 //Mostrar puntuació final
-console.table(joc1.mostrarMarcador());
+console.table(joc1.mostrarPuntuacio());
 
 //Mostrar xl guanyadorx
 console.log(joc1.mostrarGuanyadorx());
